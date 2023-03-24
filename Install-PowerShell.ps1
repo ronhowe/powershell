1b61eeedc49942d9f4a65a93081a6194cc595cd6
Ron Howe <ronhowe@hotmail.com>
2023-03-24 14:41:01 -0400

    Added Install-PowerShell.ps1

---
+++ Install-PowerShell.ps1
@@ -0,0 +1,34 @@
+#requires -RunAsAdministrator
+#requires -Module BitsTransfer
+
+[CmdletBinding()]
+param(
+    [string]$Source = "https://github.com/PowerShell/PowerShell/releases/download/v7.3.3/PowerShell-7.3.3-win-x64.msi"
+)
+
+$ErrorActionPreference = "Stop"
+
+$destination = $(Join-Path -Path $env:TEMP -ChildPath "$(New-Guid).msi")
+
+Start-BitsTransfer -Source $Source -Destination $destination
+
+$parameters = @{
+    FilePath         = "msiexec.exe"
+    ArgumentList     = @(
+        "/package",
+        $destination,
+        "/quiet",
+        "ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1",
+        "ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1",
+        "ENABLE_PSREMOTING=1",
+        "REGISTER_MANIFEST=1",
+        "USE_MU=1 ENABLE_MU=1",
+        "ADD_PATH=1"
+    )
+    NoNewWindow      = $true
+    Wait             = $true
+    WorkingDirectory = $env:TEMP
+}
+Start-Process @parameters
+
+Remove-Item -Path $destination
