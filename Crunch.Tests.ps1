00d58a01dbc5727f1e528420068c483bcf21659f
Ron Howe <ronhowe@hotmail.com>
2023-03-26 16:24:00 -0400

    Added Crunch with Tests Scripts

---
+++ Crunch.Tests.ps1
@@ -0,0 +1,16 @@
+BeforeAll {
+    . $PSCommandPath.Replace(".Tests.ps1", ".ps1")
+}
+
+Describe "Get-Planet" {
+    Context "Filter Tests" {
+        It "No Filter Returns All Planets" {
+            $allPlanets = Get-Planet
+            $allPlanets.Count | Should -Be 8
+        }
+        It "Filter Returns Appropriate Planet" {
+            $allPlanets = Get-Planet -Name "*atur*"
+            $allPlanets.Count | Should -Be 1
+        }
+    }
+}
