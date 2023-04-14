084289b54c0d64e9d0dbcacb3323cfa7f41cb2ae
Ron Howe <ronhowe@hotmail.com>
2023-04-14 13:50:25 -0400

    Updated profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -2,17 +2,27 @@ Write-Host "https://github.com/ronhowe/powershell/blob/main/profile.ps1" -Foregr
 
 Write-Verbose "PowerShell $($PSVersionTable.PSVersion.ToString())" -Verbose
 
-New-Variable -Name "Root" -Value "C:\VSTS" -Scope "Global" -Force -Verbose
-if (Test-Path -Path $Root) {
-    Set-Location -Path $Root -Verbose
+if (Test-Path -Path "C:\VSTS") {
+    Write-Verbose "Setting `$Root" -Verbose
+    New-Variable -Name "Root" -Value "C:\VSTS" -Scope "Global" -Force -Verbose
+    Set-Location -Path $Root
+}
+else {
+    Write-Verbose "Skipping `$Root" -Verbose
 }
 
 if (($host.Name -eq "Windows PowerShell ISE Host") -and (Get-Module -Name "ISESteroids" -ListAvailable)) {
     Write-Verbose "Importing ISESteroids" -Verbose
-    Import-Module -Name "ISESteroids" | Out-Null
+    Import-Module -Name "ISESteroids"
+}
+else {
+    Write-Verbose "Skipping ISESteroids" -Verbose
 }
 
 if (($host.Name -ne "Visual Studio Code Host") -and (Get-Module -Name "posh-git" -ListAvailable)) {
     Write-Verbose "Importing posh-git" -Verbose
     Import-Module -Name "posh-git"
+}
+else {
+    Write-Verbose "Skipping posh-git" -Verbose
 }
