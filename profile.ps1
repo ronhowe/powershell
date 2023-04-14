1cfe590eb17b54db3ee99faa9940000fe37fade0
Ron Howe <rhowe@idibilling.com>
2023-04-14 12:53:34 -0400

    Updated profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,13 +1,18 @@
-Write-Host "Universal PowerShell Profile" -ForegroundColor Green
+Write-Host "https://github.com/ronhowe/powershell/blob/main/profile.ps1" -ForegroundColor Green
+
+Write-Verbose "PowerShell $($PSVersionTable.PSVersion.ToString())" -Verbose
 
 New-Variable -Name "Root" -Value "C:\VSTS" -Scope "Global" -Force -Verbose
 if (Test-Path -Path $Root) {
     Set-Location -Path $Root -Verbose
 }
 
-Write-Verbose "PowerShell $($PSVersionTable.PSVersion.ToString())"
-
 if (($PSVersionTable.PSEdition -eq "Desktop") -and (Get-Module -Name "ISESteroids" -ListAvailable)) {
     Write-Verbose "Importing ISESteroids" -Verbose
-    Import-Module -Name "ISESteroids"
+    Import-Module -Name "ISESteroids" | Out-Null
+}
+
+if (($host.Name -ne "Visual Studio Code Host") -and (Get-Module -Name "posh-git" -ListAvailable)) {
+    Write-Verbose "Importing posh-git" -Verbose
+    Import-Module -Name "posh-git"
 }
