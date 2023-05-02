f2c00c716483975e179154c007ad85b025cb55ac
Ron Howe <ronhowe@hotmail.com>
2023-05-02 16:48:36 -0400

    Refactored API Tests

--- azure/Test-Api.ps1
+++ azure/Test-Api.ps1
@@ -1,15 +1,25 @@
+[CmdletBinding()]
 param(
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
     [Uri]$Uri = "https://localhost:444",
 
+    [Parameter(Mandatory = $false)]
     [switch]$Loop,
 
+    [Parameter(Mandatory = $false)]
     [int]$Seconds = 1
 )
 
-$container = New-PesterContainer -Path "$HOME\repos\ronhowe\powershell\azure\Api.Tests.ps1" -Data @{ Uri = "https://localhost:444" }
+$path = "$HOME\repos\ronhowe\powershell\azure\Api.Tests.ps1"
+
+[hashtable]$data = @{
+    "Uri" = "https://localhost:444"
+}
+$container = New-PesterContainer -Path $path -Data $data
 
 do {
-    Invoke-Pester -Path "$HOME\repos\ronhowe\powershell\azure\Api.Tests.ps1" -Output Detailed -Container $container
+    Invoke-Pester -Path $path -Output Detailed -Container $container
 
     if ($Loop) {
         Start-Sleep -Seconds $Seconds
--- azure/Test-ApiKestrel.ps1
+++ azure/Test-ApiKestrel.ps1
@@ -1,3 +1,4 @@
+[CmdletBinding()]
 param(
     [switch]$Loop,
 
