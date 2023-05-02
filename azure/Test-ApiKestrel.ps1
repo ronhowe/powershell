5d4b96d257120d8dc4bd93cb97683ea56dbc250c
Ron Howe <ronhowe@hotmail.com>
2023-05-02 17:05:49 -0400

    Refactored API Tests

--- azure/Test-ApiKestrel.ps1
+++ azure/Test-ApiKestrel.ps1
@@ -1,8 +1,4 @@
-[CmdletBinding()]
-param(
-    [switch]$Loop,
-
-    [int]$Seconds = 1
-)
-
-& "$PSScriptRoot/Test-Api.ps1" -Uri "https://localhost:444" -Loop:$Loop -Seconds $Seconds
+while ($true) {
+    Invoke-Pester -Path "$PSScriptRoot\Api.Tests.ps1" -Output Detailed -Container (New-PesterContainer -Path "$PSScriptRoot\Api.Tests.ps1" -Data @{ "Uri" = "https://localhost:444" })
+    Start-Sleep -Seconds 1
+}
