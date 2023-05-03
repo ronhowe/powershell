d001989a26eec007d90b906e87fb95efee8f838a
Ron Howe <ronhowe@hotmail.com>
2023-05-02 22:55:21 -0400

    Reorganized Files

--- Assert-RunAsAdministrator.ps1
+++
@@ -1,7 +0,0 @@
-function Assert-RunAsAdministrator {
-    $identity = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent())
-
-    if (-not $identity.IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-        throw "Process is not running with administrator privileges."
-    }
-}
---
+++ tools/Assert-RunAsAdministrator.ps1
@@ -0,0 +1,5 @@
+$identity = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent())
+
+if (-not $identity.IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+    throw "Process is not running with administrator privileges."
+}
