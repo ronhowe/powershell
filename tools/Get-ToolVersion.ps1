523bd4905c720f3d113d0caafeb1ca167ec9ee03
Ron Howe <ronhowe@hotmail.com>
2023-05-11 16:39:01 -0400

    Updated Get-ToolVersion

--- tools/Get-ToolVersion.ps1
+++ tools/Get-ToolVersion.ps1
@@ -1,6 +1,15 @@
+#requires -module "WriteAscii"
+Write-Ascii "az" -ForegroundColor Green
 az --version
+Write-Ascii "bicep" -ForegroundColor Green
 bicep --version
+Write-Ascii "code" -ForegroundColor Green
+code --version
+Write-Ascii "dotnet" -ForegroundColor Green
 dotnet --version
+Write-Ascii "gh" -ForegroundColor Green
 gh --version
+Write-Ascii "git" -ForegroundColor Green
 git --version
+Write-Ascii "wsl" -ForegroundColor Green
 wsl --version
