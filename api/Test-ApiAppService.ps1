45a3a9839135e4ce040254eb7b4caafa5d73d56e
Ron Howe <ronhowe@hotmail.com>
2023-05-13 07:48:28 -0400

    Updated API App Service Test

--- api/Test-ApiAppService.ps1
+++ api/Test-ApiAppService.ps1
@@ -3,7 +3,7 @@ while ($true) {
     Clear-Host
     $path = "$PSScriptRoot\Api.Tests.ps1"
     $data = @(
-        @{ Name = "App Service" ; Uri = "https://app-ronhowe-000.azurewebsites.net:443"; CustomHeader = "app-ronhowe-000" }
+        @{ Name = "App Service" ; Uri = "https://app-ronhowe-000.azurewebsites.net:443"; CustomHeader = "appcs-ronhowe-000" }
     )
     Invoke-Pester -Path $path -Output Detailed -Container (New-PesterContainer -Path $path -Data $data)
     Write-Ascii $data.Name
