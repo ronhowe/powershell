c779ca2b04245ac7759c7719843cd1fcd637f041
Ron Howe <ronhowe@hotmail.com>
2024-04-22 10:22:51 -0400

    Updated Debug POST

--- debug/Debug-PowerOnSelfTest.ps1
+++ debug/Debug-PowerOnSelfTest.ps1
@@ -1,14 +1,14 @@
 [CmdletBinding()]
 param (
-    [string]
-    $Value = "value"
+    [guid]
+    $Value = (New-Guid)
 )
 begin {
-    Write-Debug "Power-On Self-Test (1 of 5) => Debug Preference $DebugPreference" -Debug
-    Write-Verbose "Power-On Self-Test (2 of 5) => Verbose Preference $VerbosePreference" -Verbose
-    Write-Information "Power-On Self-Test (3 of 5) => Information Preference $InformationPreference" -InformationAction Continue
-    Write-Warning "Power-On Self-Test (4 of 5) => Warning Preference $WarningPreference" -WarningAction Continue
-    Write-Error "Power-On Self-Test (5 of 5) => Error Preference $ErrorActionPreference" -ErrorAction Continue
+    Write-Debug "Power-On Self-Test (1 of 5) => `$DebugPreference = $DebugPreference" -Debug
+    Write-Verbose "Power-On Self-Test (2 of 5) => `$VerbosePreference = $VerbosePreference" -Verbose
+    Write-Information "Power-On Self-Test (3 of 5) => `$InformationPreference = $InformationPreference" -InformationAction Continue
+    Write-Warning "Power-On Self-Test (4 of 5) => `$WarningPreference = $WarningPreference" -WarningAction Continue
+    Write-Error "Power-On Self-Test (5 of 5) => `$ErrorActionPreference = $ErrorActionPreference" -ErrorAction Continue
 
     Write-Debug "Begin $($MyInvocation.MyCommand.Name)"
 
@@ -20,6 +20,10 @@ process {
     $ErrorActionPreference = "Stop"
 
     Write-Debug "Process $($MyInvocation.MyCommand.Name)"
+
+    Write-Verbose "Writing Output"
+
+    Write-Output $Value
 
     Write-Host "OK" -ForegroundColor Green
 }
