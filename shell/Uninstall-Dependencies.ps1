4e821b1489747d06175a01fdced95682feb8b1c6
Ron Howe <ronhowe@hotmail.com>
2024-05-15 14:36:27 -0400

    Added Uninstall Dependencies Script

--- shell/Uninstall-Dependencies.ps1
+++ shell/Uninstall-Dependencies.ps1
@@ -1,16 +1,40 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $true)]
-    [ValidateNotNullOrEmpty()]
-    [string]$ModuleName
 )
+begin {
+    Write-Debug "Begin $($MyInvocation.MyCommand.Name)"
 
-$allVersions = Get-InstalledModule $ModuleName -AllVersions
+    Get-Variable -Scope "Local" -Include @($MyInvocation.MyCommand.Parameters.Keys) |
+    Select-Object -Property @("Name", "Value") |
+    ForEach-Object { Write-Debug "`$$($_.Name)=$($_.Value)" }
+}
+process {
+    $ErrorActionPreference = "Stop"
 
-$latestVersion = $allVersions |
-Sort-Object Version |
-Select-Object -Last 1
+    Write-Debug "Process $($MyInvocation.MyCommand.Name)"
 
-$allVersions |
-Where-Object { $_.Version -ne $latestVersion.Version } |
-ForEach-Object { Uninstall-Module -Name $ModuleName -RequiredVersion $_.Version }
+    Write-Verbose "Importing Dependencies Definition"
+    (Import-PowerShellDataFile -Path "$PSScriptRoot\Dependencies.psd1").Modules |
+    ForEach-Object {
+        $moduleName = $_.Name
+        $moduleVersion = $_.Version
+        Get-InstalledModule -Name $moduleName -AllVersions |
+        Where-Object { $_.Version -ne $moduleVersion } |
+        Sort-Object -Property "Version" |
+        ForEach-Object {
+            if ($_.Name -ne "Pester" -and $_.Name -ne "PSReadLine") {
+                Write-Host "Uninstalling @{ ModuleName = $($_.Name) ; RequiredVersion = $($_.Version) }... " -NoNewline
+                Uninstall-Module -Name $_.Name -RequiredVersion $_.Version
+                Write-Host "OK" -ForegroundColor Green
+            }
+            else {
+                Write-Warning "Skipping @{ ModuleName = $($_.Name) ; RequiredVersion = $($_.Version) }" -WarningAction Continue
+            }
+        }
+    }
+
+    Write-Host "OK" -ForegroundColor Green
+}
+end {
+    Write-Debug "End $($MyInvocation.MyCommand.Name)"
+}
