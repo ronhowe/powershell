50c38d6c27d757d62f601dbf0834c98faa523855
Ron Howe <ronhowe@hotmail.com>
2024-11-25 11:27:05 -0500

    DSC

--- dsc/Debug-Dsc.ps1
+++ dsc/Debug-Dsc.ps1
@@ -8,26 +8,31 @@ Clear-Host
 Import-Module -Name "Pester"
 
 Clear-Host
-Invoke-Pester -Path ".\DevBoxConfiguration.Tests.ps1" -Output Detailed
+.\Invoke-DevBoxConfiguration.ps1
+
+Clear-Host
+.\Invoke-DevBoxConfiguration.Tests.ps1
 
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.diagnostics/?view=powershell-5.1
 Clear-Host
 Import-Module -Name "Microsoft.PowerShell.Diagnostics"
 
-# Get-WinEvent : Could not retrieve information about the Microsoft-Windows-RemoteDesktopServices-RemoteFX-Synth3dvsp provider. Error: The specified resource type cannot be found in the image file.
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.diagnostics/get-winevent?view=powershell-5.1
 Clear-Host
 Get-WinEvent -ListProvider * -ErrorAction SilentlyContinue |
 Select-Object -Property @("Name") |
 Sort-Object -Property @("Name")
 
-# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.diagnostics/get-winevent?view=powershell-5.1
 Clear-Host
 $events = Get-WinEvent -LogName "Microsoft-Windows-Dsc/Operational" -MaxEvents 2
 $events |
 Select-Object -Property @("TimeCreated", "Message")
 
 $jobId = Read-Host -Prompt "Enter Job ID"
-
 Get-Content -Path "C:\Windows\system32\configuration\ConfigurationStatus\{$jobId}-0.details.json." -Raw
 
-Get-DscLocalConfigurationManager
+Get-DscLocalConfigurationManager -Verbose
+Get-DscConfigurationStatus -Verbose
+Get-DscConfiguration -Verbose
+Test-DscConfiguration -Verbose
+Start-DscConfiguration -UseExisting -Wait -Verbose
