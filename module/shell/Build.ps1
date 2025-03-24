2fb7805c6e6145b79d585cf95c4732f63944e382
Ron Howe <ronhowe@hotmail.com>
2025-03-24 12:24:50 -0400

    Improved Build

--- module/shell/Build.ps1
+++ module/shell/Build.ps1
@@ -1,7 +1,9 @@
+#region requires
 #requires -Module "Configuration"
 #requires -Module "Metadata"
 #requires -Module "InvokeBuild"
 #requires -Module "ModuleBuilder"
+#endregion requires
 
 [CmdletBinding()]
 param(
@@ -9,32 +11,48 @@ param(
     [string]
     $Version = "0.0.0"
 )
+begin {}
+process {
+    #region tasks
+    task full clean, build, import
+    task . build
+    #endregion tasks
 
-task rebuild clean, build
-task . clean, build
-
-task clean {
-    Write-Verbose "Removing Module"
-    Get-Module -Name "Shell" |
-    Remove-Module -Force
-
-    Write-Verbose "Removing Output"
-    Remove-Item -Path "$PSScriptRoot\bin" -Recurse -Force -ErrorAction SilentlyContinue
-}
-
-task build {
-    Write-Verbose "Building Module"
-    $parameters = @{
-        CopyPaths                  = @(
-            "$PSScriptRoot\source\Shell.json",
-            "$PSScriptRoot\source\Shell.nuspec",
-            "$PSScriptRoot\source\LICENSE*"
-        )
-        OutputDirectory            = "$PSScriptRoot\bin"
-        SourcePath                 = "$PSScriptRoot\source\Shell.psd1"
-        UnversionedOutputDirectory = $true
-        Version                    = $Version
+    #region build
+    task build {
+        Write-Verbose "Building Module"
+        $parameters = @{
+            CopyPaths                  = @(
+                "$PSScriptRoot\source\Shell.json",
+                "$PSScriptRoot\source\Shell.nuspec",
+                "$PSScriptRoot\source\LICENSE*"
+            )
+            OutputDirectory            = "$PSScriptRoot\bin"
+            SourcePath                 = "$PSScriptRoot\source\Shell.psd1"
+            UnversionedOutputDirectory = $true
+            Version                    = $Version
+        }
+        Build-Module @parameters |
+        Out-Null
     }
-    Build-Module @parameters |
-    Out-Null
+    #endregion  build
+
+    #region clean
+    task clean {
+        Write-Verbose "Removing Module"
+        Get-Module -Name "Shell" |
+        Remove-Module -Force
+
+        Write-Verbose "Removing Output"
+        Remove-Item -Path "$PSScriptRoot\bin" -Recurse -Force -ErrorAction SilentlyContinue
+    }
+    #endregion clean
+
+    #region import
+    task import {
+        Write-Verbose "Importing Module"
+        Import-Module -Name "$PSScriptRoot\bin\Shell" -Global -Force
+    }
+    #endregion import
 }
+end {}
--- module/shell/Debug-Build.ps1
+++ module/shell/Debug-Build.ps1
@@ -1,1 +1,15 @@
-Invoke-Build -File "$PSScriptRoot\Build.ps1" -Task "rebuild" -Verbose
+[CmdletBinding()]
+param(
+)
+begin {
+    Write-Debug "Beginning $($MyInvocation.MyCommand.Name)"
+}
+process {
+    Write-Debug "Processing $($MyInvocation.MyCommand.Name)"
+
+    Write-Verbose "Invoking Build"
+    Invoke-Build -File "$PSScriptRoot\Build.ps1" -Task "full" -Verbose
+}
+end {
+    Write-Debug "Ending $($MyInvocation.MyCommand.Name)"
+}
