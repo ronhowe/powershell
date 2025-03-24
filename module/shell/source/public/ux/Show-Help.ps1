092503f838c2f7805cac36942a606026c1755642
Ron Howe <ronhowe@hotmail.com>
2025-03-24 16:30:44 -0400

    Improved Help

--- module/shell/source/public/ux/Show-Help.ps1
+++ module/shell/source/public/ux/Show-Help.ps1
@@ -8,23 +8,20 @@ function Show-Help {
     process {
         Write-Debug "Processing $($MyInvocation.MyCommand.Name)"
 
-        # sync with Aliases.ps1
-        # sync with Aliases.Tests.ps1
-        # sync with Show-Help.ps1
-        Write-Host "catfact`n`t- Displays a random fact about cats."
-        Write-Host "date`n`t- Displays local and UTC times."
-        Write-Host "help`n`t- Displays the Shell help."
-        Write-Host "home`n`t- Locates the standard home folder."
-        Write-Host "matrix`n`t- Take the blue pill or red pill?  (requires wsl and cmatrix)"
-        Write-Host "menu`n`t- Starts the Shell menu"
-        Write-Host "new`n`t- Refreshes the screen."
-        Write-Host "oops`n`t- Opens Notepad to edit command history."
-        Write-Host "redact`n`t- Removes the last issued command from the PowerShell history."
-        Write-Host "repos`n`t- Locates the standard repos folder."
-        Write-Host "shell`n`t- Starts the Shell."
-        Write-Host "time`n`t- Displays local and UTC times."
-        Write-Host "version`n`t- Displays the version of PowerShell and the Shell."
-        Write-Host "weather`n`t- Displays the weather."
+        Write-Host "catfact => Displays a random fact about cats."
+        Write-Host "date    => Displays local and UTC times."
+        Write-Host "help    => Displays the Shell help."
+        Write-Host "home    => Locates the standard home folder."
+        Write-Host "matrix  => Take the blue pill or red pill?  (requires wsl and cmatrix)"
+        Write-Host "menu    => Starts the Shell menu"
+        Write-Host "new     => Refreshes the screen."
+        Write-Host "oops    => Opens Notepad to edit command history."
+        Write-Host "redact  => Removes the last issued command from the PowerShell history."
+        Write-Host "repos   => Locates the standard repos folder."
+        Write-Host "shell   => Starts the Shell."
+        Write-Host "time    => Displays local and UTC times."
+        Write-Host "version => Displays the version of PowerShell and the Shell."
+        Write-Host "weather => Displays the weather."
     }
     end {
         Write-Debug "Ending $($MyInvocation.MyCommand.Name)"
