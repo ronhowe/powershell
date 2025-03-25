5eb2f0525ff3031c80d1b6a3dd095e04aa27ba63
Ron Howe <ronhowe@hotmail.com>
2025-03-25 19:28:08 -0400

    Improved Publish DSC Certificate Script

--- dsc/lab/Publish-DscEncryptionCertificate.ps1
+++ dsc/lab/Publish-DscEncryptionCertificate.ps1
@@ -1,56 +1,52 @@
-function Publish-DscEncryptionCertificate {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [ValidateNotNullorEmpty()]
-        [string[]]
-        $Nodes,
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [ValidateNotNullorEmpty()]
+    [string[]]
+    $Nodes,
     
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullorEmpty()]
-        [pscredential]
-        $Credential,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullorEmpty()]
+    [pscredential]
+    $Credential,
     
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullorEmpty()]
-        [string]
-        $PfxPath,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullorEmpty()]
+    [string]
+    $PfxPath,
     
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullorEmpty()]
-        [securestring]
-        $PfxPassword
-    )
-    begin {
-        Write-Verbose "Beginning $($MyInvocation.MyCommand.Name)"
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullorEmpty()]
+    [securestring]
+    $PfxPassword
+)
+begin {
+    Write-Debug "Beginning $($MyInvocation.MyCommand.Name)"
+}
+process {
+    Write-Debug "Processing $($MyInvocation.MyCommand.Name)"
 
-        Get-Variable -Scope "Local" -Include @($MyInvocation.MyCommand.Parameters.Keys) |
-        Select-Object -Property @("Name", "Value") |
-        ForEach-Object { Write-Debug "`$$($_.Name) = $($_.Value)" }
-    }
-    process {
-        Write-Verbose "Processing $($MyInvocation.MyCommand.Name)"
+    foreach ($node in $Nodes) {
+        Write-Verbose "Getting PSSession To $node"
+        $session = New-PSSession -ComputerName $node -Credential $Credential
 
-        foreach ($node in $Nodes) {
-            Write-Verbose "Getting PSSession To $node"
-            $session = New-PSSession -ComputerName $node -Credential $Credential
+        Write-Verbose "Copying PFX To $node"
+        Copy-Item -Path $PfxPath -Destination "C:\DscPrivateKey.pfx" -ToSession $session
 
-            Write-Verbose "Copying PFX To $node"
-            Copy-Item -Path $PfxPath -Destination "C:\DscPrivateKey.pfx" -ToSession $session
+        Write-Verbose "Importing PFX On $node"
+        $ScriptBlock = {
+            Import-PfxCertificate -FilePath "C:\DscPrivateKey.pfx" -CertStoreLocation "Cert:\LocalMachine\My" -Password $using:PfxPassword |
+            Out-Null
 
-            Write-Verbose "Importing PFX On $node"
-            $ScriptBlock = {
-                Import-PfxCertificate -FilePath "C:\DscPrivateKey.pfx" -CertStoreLocation "Cert:\LocalMachine\My" -Password $using:PfxPassword |
-                Out-Null
-            }
-            Invoke-Command -Session $session -ScriptBlock $ScriptBlock
-
-            Write-Verbose "Removing PSSession To $node"
-            $session |
-            Remove-PSSession
+            Remove-Item -Path -Path "C:\DscPrivateKey.pfx"
         }
+        Invoke-Command -Session $session -ScriptBlock $ScriptBlock
+
+        Write-Verbose "Removing PSSession To $node"
+        $session |
+        Remove-PSSession
     }
-    end {
-        Write-Verbose "Ending $($MyInvocation.MyCommand.Name)"
-    }
+}
+end {
+    Write-Debug "Ending $($MyInvocation.MyCommand.Name)"
 }
