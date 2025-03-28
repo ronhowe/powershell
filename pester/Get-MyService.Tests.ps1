4a2e7e21022cf2597421878619d6b09d2b3537ea
Ron Howe <ronhowe@hotmail.com>
2025-03-28 06:25:46 -0400

    Improved Pester Tests

--- pester/Get-MyService.Tests.ps1
+++ pester/Get-MyService.Tests.ps1
@@ -4,12 +4,10 @@ BeforeAll {
 Describe "Get-MyService Tests" {
     Context "Testing MyInput" {
         It "Asserting MyService Returns $false" {
-            Write-Host "Asserting MyService Returns $false" -ForegroundColor Cyan
             Get-MyService -MyInput $false |
             Should -BeFalse
         }
         It "Asserting MyService Returns $true" {
-            Write-Host "Asserting MyService Returns $true" -ForegroundColor Cyan
             Get-MyService -MyInput $true |
             Should -BeTrue
         }
