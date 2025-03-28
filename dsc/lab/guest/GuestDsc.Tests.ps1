64412b9e701f763c47cdc2e7f6d17fb179069cf8
Ron Howe <ronhowe@hotmail.com>
2025-03-28 10:39:37 -0400

    Improved Lab DSC

--- dsc/lab/guest/GuestDsc.Tests.ps1
+++ dsc/lab/guest/GuestDsc.Tests.ps1
@@ -1,50 +1,54 @@
-$TestCases = @(
-    Get-VM -Name "LAB*" |
-    ForEach-Object { [hashtable]@{ "Node" = $_.Name } }
-)
 Describe "Guest Dsc Tests" {
-    Context "Network" {
-        It "Asserting Ping Connectivity To <Node>" -TestCases $TestCases {
-            param (
-                [string]
-                $Node
-            )
-            Write-Host "Asserting Ping Connectivity To $Node" -ForegroundColor Cyan
-            (Test-NetConnection -ComputerName $Node -WarningAction SilentlyContinue).PingSucceeded |
+    Context "Network Tests" {
+        It "Asserting Ping Connectivity To <Name>" -ForEach ` @(
+            Get-VM -Name "LAB*" |
+            ForEach-Object { [hashtable]@{ "Name" = $_.Name } }
+        ) {
+            (Test-NetConnection -ComputerName $Name -WarningAction SilentlyContinue).PingSucceeded |
             Should -BeTrue
         }
     }
-}
-$TestCases = @(
-    Get-VM -Name "LAB*SQL*" |
-    ForEach-Object { [hashtable]@{ "Node" = $_.Name } }
-)
-Describe "Guest SQL Server Tests" {
-    Context "Network" {
-        It "Asserting Net Connection On Port 1433 To <Node>" -TestCases $TestCases {
-            param (
-                [string]
-                $Node
-            )
-            Write-Host "Asserting Net Connection On Port 1433 To $Node" -ForegroundColor Cyan
-            (Test-NetConnection -ComputerName $Node -Port 1433 -WarningAction SilentlyContinue).TcpTestSucceeded |
+    Context "RDP Tests" {
+        It "Asserting RDP Connectivity On Poprt 3389 To <Name>" -ForEach ` @(
+            Get-VM -Name "LAB*" |
+            ForEach-Object { [hashtable]@{ "Name" = $_.Name } }
+        ) {
+            (Test-NetConnection -ComputerName $Name -Port 3389 -WarningAction SilentlyContinue).TcpTestSucceeded |
             Should -BeTrue
         }
     }
-}
-$TestCases = @(
-    Get-VM -Name "LAB*WEB*" |
-    ForEach-Object { [hashtable]@{ "Node" = $_.Name } }
-)
-Describe "Guest Web Server Tests" {
-    Context "Network" {
-        It "Asserting Net Connection On Port 80 To <Node>" -TestCases $TestCases {
-            param (
-                [string]
-                $Node
-            )
-            Write-Host "Asserting Net Connection On Port 80 To $Node" -ForegroundColor Cyan
-            (Test-NetConnection -ComputerName $Node -Port 80 -WarningAction SilentlyContinue).TcpTestSucceeded |
+    Context "WinRM Tests" {
+        It "Asserting WinRM Connectivity On Port 5985 To <Name>" -ForEach ` @(
+            Get-VM -Name "LAB*" |
+            ForEach-Object { [hashtable]@{ "Name" = $_.Name } }
+        ) {
+            (Test-NetConnection -ComputerName $Name -Port 5985 -WarningAction SilentlyContinue).TcpTestSucceeded |
+            Should -BeTrue
+        }
+        ## TODO: Enable HTTPS Listener On Port 5986
+        # It "Asserting WinRM Connectivity On Port 5986 To <Name>" -ForEach ` @(
+        #     Get-VM -Name "LAB*" |
+        #     ForEach-Object { [hashtable]@{ "Name" = $_.Name } }
+        # ) {
+        #     (Test-NetConnection -ComputerName $Name -Port 5986 -WarningAction SilentlyContinue).TcpTestSucceeded |
+        #     Should -BeTrue
+        # }
+    }
+    Context "SQL Tests" {
+        It "Asserting SQL Connectivity On Port 1433 To <Name>" -ForEach ` @(
+            Get-VM -Name "LAB*SQL*" |
+            ForEach-Object { [hashtable]@{ "Name" = $_.Name } }
+        ) {
+            (Test-NetConnection -ComputerName $Name -Port 1433 -WarningAction SilentlyContinue).TcpTestSucceeded |
+            Should -BeTrue
+        }
+    }
+    Context "Web Tests" {
+        It "Asserting HTTP Connectivity On Port 80 To <Name>" -ForEach ` @(
+            Get-VM -Name "LAB*WEB*" |
+            ForEach-Object { [hashtable]@{ "Name" = $_.Name } }
+        ) {
+            (Test-NetConnection -ComputerName $Name -Port 80 -WarningAction SilentlyContinue).TcpTestSucceeded |
             Should -BeTrue
         }
     }
