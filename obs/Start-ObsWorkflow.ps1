32a32c1d772b9f2ccab7becd5690fecda8e4a88b
Ron Howe <ronhowe@hotmail.com>
2025-03-28 15:31:20 -0400

    Improved OBS Script

--- obs/Start-ObsWorkflow.ps1
+++ obs/Start-ObsWorkflow.ps1
@@ -17,6 +17,10 @@ param (
 )
 begin {
     Write-Debug "Begin $($MyInvocation.MyCommand.Name)"
+
+    Get-Variable -Scope "Local" -Include @($MyInvocation.MyCommand.Parameters.Keys) |
+    Select-Object -Property @("Name", "Value") |
+    ForEach-Object { Write-Debug "`$$($_.Name) = $($_.Value)" }
 }
 process {
     Write-Debug "Process $($MyInvocation.MyCommand.Name)"
