084f0e533ffc1bdbd8377e4aaf5901eeb4d05912
Ron Howe <ronhowe@hotmail.com>
2025-03-29 06:39:28 -0400

    Improved Developer Scripts

--- developer/Get-DevOpsStatus.ps1
+++ developer/Get-DevOpsStatus.ps1
@@ -7,6 +7,10 @@ param(
 )
 begin {
     Write-Debug "Beginning $($MyInvocation.MyCommand.Name)"
+
+    Get-Variable -Scope "Local" -Include @($MyInvocation.MyCommand.Parameters.Keys) |
+    Select-Object -Property @("Name", "Value") |
+    ForEach-Object { Write-Debug "`$$($_.Name) = $($_.Value)" }
 }
 process {
     Write-Debug "Processing $($MyInvocation.MyCommand.Name)"
