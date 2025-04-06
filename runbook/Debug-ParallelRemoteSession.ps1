5e246439d1fba88e94f43ffc26d1c16482e72daa
Ron Howe <ronhowe@hotmail.com>
2025-04-06 10:10:47 -0400

    Improved Debug Parallel Remote Session Scripts

--- runbook/Debug-ParallelRemoteSession.ps1
+++ runbook/Debug-ParallelRemoteSession.ps1
@@ -1,47 +1,89 @@
-$credential = Get-Credential
+$computers = @("LAB-DC-00")
 $computers = @("LAB-APP-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00")
-$computers = @("LAB-APP-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "NOT-EXISTING-00")
-$computers = @("LAB-APP-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "NOT-EXISTING-00", "localhost")
-$computers = @("LAB-APP-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00")
+$computers = @("LAB-APP-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "NON-EXISTENT", "LOCALHOST")
+$computers = @("LAB-APP-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00") * 5
+$computers = @("LAB-APP-00", "LAB-DC-00", "LAB-SQL-00", "LAB-WEB-00", "NON-EXISTENT", "LOCALHOST") * 5
+$credential = Get-Credential
 $thumbprint = "F59627BB6A2B553E68A5565A5DFA639CB91574DE"
 
-$computers |
-ForEach-Object {
-    $session = New-PSSession -ComputerName $_ -Credential $credential -ErrorAction Continue
-    if ($session) {
-        Invoke-Command -Session $session -ScriptBlock {
-            Get-ChildItem -Path "Cert:\LocalMachine\My\$thumbprint" |
-            Select-Object -First 1 |
-            Select-Object -Property "Thumbprint", "NotBefore", "NotAfter", "Subject", "Issuer"
-        } -ErrorAction Continue
-    }
-    $session | Remove-PSSession
+#region Serial With Script Block
+Measure-Command {
+    $computers |
+    ForEach-Object {
+        $session = New-PSSession -ComputerName $_ -Credential $credential
+        if ($session) {
+            Invoke-Command -Session $session -ScriptBlock {
+                Get-ChildItem -Path "Cert:\LocalMachine\My\$thumbprint" |
+                Select-Object -First 1 |
+                Select-Object -Property "Thumbprint", "NotBefore", "NotAfter", "Subject", "Issuer"
+            }
+            Remove-PSSession -Session $session
+        }
+    } |
+    Select-Object -Property @(@{Name = "ComputerName"; Expression = { $_.PSComputerName } }, "Thumbprint", "NotAfter") |
+    Format-Table -AutoSize
 } |
-Select-Object -Property @(@{Name = "ComputerName"; Expression = { $_.PSComputerName } }, "Thumbprint", "NotAfter") |
 Format-Table -AutoSize
+#endregion Serial With Script Block
 
-# by script block
-$computers |
-ForEach-Object -Parallel {
-    $thumbprint = $using:thumbprint
-    $session = New-PSSession -ComputerName $_ -Credential $using:credential -ErrorAction Continue
-    if ($session) {
-        Invoke-Command -Session $session -ScriptBlock {
-            Get-ChildItem -Path "Cert:\LocalMachine\My\$using:thumbprint" |
-            Select-Object -Property "Thumbprint", "NotBefore", "NotAfter", "Subject", "Issuer"
-        } -ErrorAction Continue
-    }
-} -ThrottleLimit 20 |
-Select-Object -Property @(@{Name = "ComputerName"; Expression = { $_.PSComputerName } }, "Thumbprint", "NotAfter") |
+#region Serial With File
+Measure-Command {
+    $computers |
+    ForEach-Object {
+        $session = New-PSSession -ComputerName $_ -Credential $credential
+        if ($session) {
+            $parameters = @{
+                Session      = $session
+                FilePath     = "D:\repos\ronhowe\powershell\runbook\Debug-ParallelRemoteSessionScript.ps1"
+                ArgumentList = @($thumbprint)
+            }
+            Invoke-Command @parameters
+            Remove-PSSession -Session $session
+        }
+    } |
+    Select-Object -Property @(@{Name = "ComputerName"; Expression = { $_.PSComputerName } }, "Thumbprint", "NotAfter") |
+    Format-Table -AutoSize
+} |
 Format-Table -AutoSize
+#endregion Serial With File
 
-# by file
-$computers |
-ForEach-Object -Parallel {
-    $session = New-PSSession -ComputerName $_ -Credential $using:credential -ErrorAction Continue
-    if ($session) {
-        Invoke-Command -Session $session -FilePath "D:\repos\ronhowe\powershell\runbook\Debug-ParallelRemoteSessionScript.ps1" -ArgumentList $using:thumbprint -ErrorAction Continue -Verbose
-    }
-} -ThrottleLimit 20 |
-Select-Object -Property @(@{Name = "ComputerName"; Expression = { $_.PSComputerName } }, "Thumbprint", "NotAfter") |
+#region Parallel With Script Block
+Measure-Command {
+    $computers |
+    ForEach-Object -Parallel {
+        $thumbprint = $using:thumbprint
+        $session = New-PSSession -ComputerName $_ -Credential $using:credential
+        if ($session) {
+            Invoke-Command -Session $session -ScriptBlock {
+                Get-ChildItem -Path "Cert:\LocalMachine\My\$using:thumbprint" |
+                Select-Object -Property "Thumbprint", "NotBefore", "NotAfter", "Subject", "Issuer"
+            }
+            Remove-PSSession -Session $session
+        }
+    } -ThrottleLimit 20 |
+    Select-Object -Property @(@{Name = "ComputerName"; Expression = { $_.PSComputerName } }, "Thumbprint", "NotAfter") |
+    Format-Table -AutoSize
+} |
 Format-Table -AutoSize
+#endregion Parallel With Script Block
+
+#region Parallel With File
+Measure-Command {
+    $computers |
+    ForEach-Object -Parallel {
+        $session = New-PSSession -ComputerName $_ -Credential $using:credential
+        if ($session) {
+            $parameters = @{
+                Session      = $session
+                FilePath     = "D:\repos\ronhowe\powershell\runbook\Debug-ParallelRemoteSessionScript.ps1"
+                ArgumentList = @($using:thumbprint)
+            }
+            Invoke-Command @parameters
+            Remove-PSSession -Session $session
+        }
+    } -ThrottleLimit 20 |
+    Select-Object -Property @(@{Name = "ComputerName"; Expression = { $_.PSComputerName } }, "Thumbprint", "NotAfter") |
+    Format-Table -AutoSize
+} |
+Format-Table -AutoSize
+#endregion Parallel With File
